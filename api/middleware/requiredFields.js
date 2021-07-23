cb51acf8c966a49d5170d8b809f0e843e080c3e9
AdrianHDev <adrianhensondev@gmail.com>
2021-07-23 12:34:38 -0400

    tests wip

--- api/middleware/requiredFields.js
+++ api/middleware/requiredFields.js
@@ -2,7 +2,7 @@ module.exports = (req, res, next) => {
     if (!req.body.username || !req.body.password) {
         next({status: 400, message: "username and password required"})
     } else {
-        req.user = { username: req.body.username.trim(), password: req.body.password.trim() }
+        req.user = { username: req.body.username, password: req.body.password }
         next()
     }
 }
--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,45 @@
+const db = require("../data/dbConfig");
+const server = require("./server");
+const request = require("supertest");
+const bcrypt = require("bcryptjs");
+
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+
+const user1 = { username: "user", password: "pass1234" };
+
+test("sanity", () => {
+  expect(true).toBe(true);
+});
+
+beforeAll(async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+});
+beforeEach(async () => {
+  await db("users").truncate();
+});
+
+afterAll(async () => {
+  await db.destroy();
+});
+describe("Verify Endpoints are working", () => {
+  it("[POST] /api/auth/register creates new User", async () => {
+    const newUser = await request(server)
+      .post("/api/auth/register")
+      .send(user1)
+      .expect(201)
+      .then((res) => res.body);
+    const user = await db("users").where({ id: newUser.id }).first();
+    expect(user);
+  });
+  it("[POST] /api/auth/register updates database", async () => {
+    const newUser = await request(server)
+      .post("/api/auth/register")
+      .send(user1)
+      .expect(201)
+      .then((res) => res.body);
+    const user = await db("users").where({ id: newUser.id }).first();
+    expect(bcrypt.compareSync(user1.password, user.password)).toBe(true);
+    expect(user.username).toBe(user1.username);
+  });
+});
