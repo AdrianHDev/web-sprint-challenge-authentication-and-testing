dbd36ec64d8adda63b203ed7ec8df032b2999104
AdrianHDev <adrianhensondev@gmail.com>
2021-07-23 11:52:32 -0400

    wip

--- api/middleware/requiredFields.js
+++ api/middleware/requiredFields.js
@@ -1,6 +1,8 @@
 module.exports = (req, res, next) => {
     if (!req.body.username || !req.body.password) {
-        req.user = { username: req.body.username.trim(), password: req.body.password.trim() }
         next({status: 400, message: "username and password required"})
-    } else {next()}
+    } else {
+        req.user = { username: req.body.username.trim(), password: req.body.password.trim() }
+        next()
+    }
 }
